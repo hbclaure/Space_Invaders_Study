b9b080138bf74ddda06fc09b71e105622fce818f
Marynel Vazquez <marynelv@stanford.edu>
2020-03-23 11:13:48 -0400

    we can now add bullets

--- space_invaders/js/space_invaders.js
+++ space_invaders/js/space_invaders.js
@@ -4,31 +4,99 @@
 
 // --- helper functions ---
 
+
+/**
+ * Create bullet
+ * @param {Phaser.Scene} scene_instance Scene instance in which to create the ship 
+ * @param {string} image_id Image ID for the ship
+ * @param {int} x horizontal coordinate for the bullet (fixed)
+ * @param {int} y vertical coordinate for the bullet (moves horizontally)
+ * @param {int} direction -1 for up, 1 for down movement
+ * @param {int} speed bullet speed (absolute value)
+ * @param {int} min_y minimum y position
+ * @param {int} max_y maximum y position
+ */
+function create_bullet(scene_instance, image_id, x, y, direction, speed = 10, min_y = 15, max_y = 600) {
+    return {
+        imageobj: scene_instance.add.image(x, y, image_id),
+        direction: direction,
+        speed: speed,
+        x: x,
+        y: y,
+        min_y: min_y,
+        max_y: max_y,
+        killed: false,
+        update: function() {
+            console.log(this.y);
+            if (!this.killed) {
+                this.y = this.y + this.speed * this.direction;
+                if (this.y < this.min_y || this.y > this.max_y) {
+                    console.log("killed bullet");
+                    this.killed = true;
+                    this.imageobj.destroy();
+                } else {
+                    this.imageobj.y = this.y;
+                }
+            }
+        },
+    };
+}
+
 /**
  * Create ship object
  * @param {Phaser.Scene} scene_instance Scene instance in which to create the ship 
  * @param {string} image_id Image ID for the ship
+ * @param {int} x horizontal coordinate for the ship (moves horizontally)
+ * @param {int} y vertical coordinate for the ship (fixed)
+ * @param {int} speed speed for the ship
+ * @param {int} min_x minimum allowed horizontal position for the ship
+ * @param {int} max_x maximum allowed horizontal position for the ship
+ * @param {int} shooting_direction shooting direction (-1 means up, 1 means down)
+ * @param {String} bullet_image_id image id for the bullet
  */
-function create_ship(scene_instance, image_id) {
+function create_ship(scene_instance, image_id="ship", x = 200, y = 540, speed = 5, min_x = 50, max_x = 750, 
+                     shooting_direction = -1, bullet_image_id="laser") {
     return {
-        imageobj: scene_instance.add.image(50, 50, image_id),
-        direction: -1,
-        speed: 5,
-        x: 200,
-        y: 540,
-        min_x: 10,
-        max_x: 740,
-        update: function(left_cursor, right_cursor) {
-            if (left_cursor && this.x > this.min_x) {
-                this.x -= this.speed;
-            } else if (right_cursor && this.x < this.max_x) {
-                this.x += this.speed;
-            }
+        scene_instance: scene_instance,                         //!< scene instance
+        imageobj: scene_instance.add.image(50, 50, image_id),   //!< image object for the ship
+        speed: speed,                                           //!< moving speed for the ship
+        x: x,                                                   //!< current x position
+        y: y,                                                   //!< current y position
+        min_x: min_x,                                           //!< minimum x position
+        max_x: max_x,                                           //!< maximum x position
+        bullets: [],                                            //!< bullets shot by the ship
+        shooting_direction: shooting_direction,                 //!< bullet direction
+        bullet_image_id: bullet_image_id,                       //!< bullet image id
+        update: function(move_left, move_right, shoot)          //!< update the ship state based on requested actions
+        {        
+            // update position
+            if (move_left) {
+                left_move = this.x - this.speed;
+                if (left_move > this.min_x) 
+                    this.x = left_move;
+            } else if (move_right) {
+                right_move = this.x + this.speed;
+                if (right_move < this.max_x) 
+                    this.x += this.speed;
+            } 
+            // update ship image based on new position
             this.imageobj.x = this.x;
             this.imageobj.y = this.y;
+            // add bullet
+            if (shoot) {
+                bullet = create_bullet(this.scene_instance, bullet_image_id, 
+                                       this.x, this.y + (30*this.shooting_direction), this.shooting_direction)
+                this.bullets.push(bullet)
+                console.log(this.bullets.length)
+            }
+            // update bullets
+            for(var i=0; i<this.bullets.length; i++) {
+                this.bullets[i].update()
+            }
         },
     };
 }
+
 
 // --- actual game ---
 
@@ -36,22 +104,25 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    // physics: {
-    //     default: 'arcade',
-    //     arcade: {
-    //         gravity: { y: 200 }
-    //     }
-    // },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    fps: 30,
 };
 
 var game = new Phaser.Game(config);
 var cursors;                            // keyboard access
+var space_key;                          // space key
 var ship1;                              // ship1
+var enemies_group1;
 
 function preload ()
 {
@@ -79,13 +150,72 @@ function preload ()
 function create ()
 {
     cursors = this.input.keyboard.createCursorKeys();
+    space_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-    ship1 = create_ship(this, "ship");
+    ship1 = create_ship(this);
 
+    this.anims.create({
+        key: 'enemy1_move',
+        frames: [
+            { key: 'enemy1_1' },
+            { key: 'enemy1_2' },
+        ],
+        frameRate: 2,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'enemy2_move',
+        frames: [
+            { key: 'enemy2_1' },
+            { key: 'enemy2_2' },
+        ],
+        frameRate: 2,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'enemy3_move',
+        frames: [
+            { key: 'enemy3_1' },
+            { key: 'enemy3_2' },
+        ],
+        frameRate: 2,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'enemy1_explote',
+        frames: [
+            { key: 'enemy1_1' },
+            { key: 'explosionpurple' },
+        ],
+        frameRate: 2,
+        repeat: 0,
+        hideOnComplete: true,
+    });
+
+    enemies_group1 = this.add.group();
+    // add purple enemies
+    for (var i=0; i<5; i++) {
+        enemy1 = this.add.sprite(400, 300, 'enemy1_1').play('enemy1_move');
+        // enemy1.setDisplaySize(30,30)
+        enemies_group1.add(enemy1);
+    }
+    // add cyan enemies
+    for (var i=0; i<5; i++) {
+        enemy2 = this.add.sprite(400, 300, 'enemy2_1').play('enemy2_move');
+        // enemy2.setDisplaySize(30,30)
+        enemies_group1.add(enemy2);
+    }
+    // align in a grid
+    Phaser.Actions.GridAlign(enemies_group1.getChildren(), 
+    { width: 5, height: 2, cellWidth: 200, cellHeight: 200, position: Phaser.Display.Align.TOP_LEFT, x: 0, y: 0 });
+
+    
 }
 
 function update ()
 {
-    ship1.update(cursors.left.isDown, cursors.right.isDown);
-
+    ship1.update(cursors.left.isDown, cursors.right.isDown, this.input.keyboard.checkDown(space_key, 500));
 }
