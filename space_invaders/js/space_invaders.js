4cf96280aa7c10d65141f208f53c19cea4299913
Marynel Vazquez <marynelv@stanford.edu>
2020-03-24 16:57:55 -0400

    added bullets to the enemies.. they can now kill the ship

--- space_invaders/js/space_invaders.js
+++ space_invaders/js/space_invaders.js
@@ -5,6 +5,104 @@
 // --- helper functions ---
 
 /**
+ * Create bullets pool
+ * @param {int} number number of bullets in the pool
+ * @param {String} image_id image id for the bullets
+ * @param {bool} increase_bbox increase the bullet bounding box by 3 times (makes it easier to detect collisions)
+ */
+function create_bullets_pool(number, image_id, increase_bbox=false) {
+    bullets = this.physics.add.group({ // bullets pool. They become visible when a bullet is fired
+        key: image_id,
+        repeat: number,
+        visible: false,
+        active: false,
+        collideWorldBounds: true,
+    }); 
+    if (increase_bbox) {
+        Phaser.Actions.Call(bullets.getChildren(), function(b) {
+            // increase bullet collision bounding box in case they are going down (makes the game a bit easier)
+            b.body.setSize(b.body.width*3 , b.body.height, true);
+        });
+    }
+    return bullets
+}
+
+/**
+ * Helper function to fire a bullet
+ * @param {Phaser.GameObjects.GameObject} bullets_group bullets group (as output by create_bullets_pool())
+ * @param {int} x horizontal coordinate for the bullet
+ * @param {int} y vertical coordinate for the bullet
+ * @param {int} direction -1 or 1 depending on whether we want the bullet to move up or down
+ * @param {int} speed magnitude of the bullet velocity
+ */
+function fire_bullet(bullets_group, x, y, direction, speed = 500) {
+    // Scans the group for the first member that has an Phaser.GameObjects.GameObject#active state set to false,
+    // and assign position x and y
+    var bullet = bullets_group.get(x, y);
+    if (bullet) {
+        // set bullet position and speed
+        bullet.body.originX = 0.5;
+        bullet.body.originY = 1.0;
+        bullet.body.velocity.y = speed * direction;
+        bullet.body.allowGravity = true;
+        // make bullet visible
+        bullet.setActive(true);
+        bullet.setVisible(true);
+        // setup callback to hide bullet when it reaches the end of the screen
+        bullet.body.onWorldBounds = true;                    
+        bullet.body.world.on('worldbounds', function(body) {
+            if (body.gameObject == bullet) {
+                bullet.setVisible(false);
+                bullet.setActive(false);
+            }
+        })
+    }
+}
+
+/**
+ * Helper function to fire a bullet from a group of enemies
+ * @param {Phaser.GameObjects.GameObject} enemies group of enemies
+ * @param {Phaser.GameObjects.GameObject} bullets group of bullets that the enemies can fire
+ */
+function fire_enemy_bullet(enemies, bullets) {
+    // find valid columns (those that still have enemies in)
+    var num_valid = 0;
+    var valid_columns = [];
+    for (var i=0; i<enemies.num_columns; i++) {
+        valid_columns.push(0);
+    }
+    var children = enemies.getChildren();
+    for (var i = 0; i < children.length; i++) {
+        col = children[i].grid_column;
+        if ([valid_columns[col]] == 0) {
+            valid_columns[col] = 1;
+            num_valid += 1;
+        }
+        if (num_valid == enemies.num_columns) break;
+    }
+    // turn valid_columns to indexes
+    var valid_index = [];
+    for (var i=0; i<valid_columns.length; i++) {
+        if (valid_columns[i] == 1)
+            valid_index.push(i);
+    }
+    // sample a colum
+    var chosen_index = Math.floor(Math.random() * num_valid);
+    var col = valid_index[chosen_index];
+    // find lowest row with an enemy for the chosen column
+    var enemy = null;
+    for (var i = 0; i < children.length; i++) {
+        if (children[i].grid_column == col && (enemy == null || enemy.grid_row < children[i].grid_row)) {
+            enemy = children[i];
+        }
+    }
+    // fire bullet for that enemy
+    if (enemy != null) {
+        fire_bullet(bullets, enemy.body.x + enemy.body.width * 0.5, enemy.body.y + 50, 1, 50);
+    }
+}
+
+/**
  * Create ship object
  * @param {string} image_id Image ID for the ship
  * @param {int} x horizontal coordinate for the ship (moves horizontally)
@@ -18,154 +116,119 @@
  */
 function create_ship(image_id="ship", x = 200, y = 540, speed = 5, bullet_image_id = "laser") {
             
-    canvas_width = this.sys.canvas.width;
-    canvas_height = this.sys.canvas.height;
-    imageobj = this.add.image(canvas_width/4, canvas_height - 10, image_id).setOrigin(0.5, 1.0); 
-    imageobj.props = {} // add properties object to ship object
-    imageobj.props.speed = speed;
-    obj_width = imageobj.displayWidth;     
+    var canvas_width = this.sys.canvas.width;
+    var canvas_height = this.sys.canvas.height;
+    var sprite = this.physics.add.sprite(canvas_width/4, canvas_height - 10, image_id).setOrigin(0.5, 1.0); 
+    sprite.body.setImmovable(true);
+    sprite.body.setAllowGravity(false);
+    sprite.body.setSize(sprite.width*0.8 , sprite.height, true);
+    sprite.props = {} // add properties object to ship sprite
+    sprite.props.speed = speed;
+    sprite.props.dead = false;
+    var obj_width = sprite.displayWidth;     
 
-    bullets = this.physics.add.group({ // bullets pool. They become visible when a bullet is fired
-        key: bullet_image_id,
-        repeat: 30,
-        visible: false,
-        active: false,
-        collideWorldBounds: true,
-    }); 
+    var bullets = this.create_bullets_pool(30, bullet_image_id);
 
     return {
-        imageobj: imageobj,                                     //!< image object for the ship
+        sprite: sprite,                                         //!< image object for the ship
         bullets_group: bullets,                                 //!< bullets shot by the ship
         update(move_left, move_right, shoot)                    //!< update the ship state based on requested actions   
-        {                
+        {          
+            // do nothing if the ship has been killed!
+            if (this.sprite.props.dead) {
+                this.sprite.alpha = 0.35;
+                return;
+            }      
             // update position
             if (move_left) {
-                this.imageobj.x = Math.max(this.imageobj.x - this.imageobj.props.speed, obj_width);
+                this.sprite.x = Math.max(this.sprite.x - this.sprite.props.speed, obj_width);
             } else if (move_right) {
-                this.imageobj.x = Math.min(this.imageobj.x + this.imageobj.props.speed, canvas_width - obj_width);
+                this.sprite.x = Math.min(this.sprite.x + this.sprite.props.speed, canvas_width - obj_width);
             } 
             // add bullet
             if (shoot) {
-                let bullet = this.bullets_group.get(this.imageobj.x, this.imageobj.y - 50);
-                if (bullet) {
-                    // set bullet position and speed
-                    bullet.body.originX = 0.5;
-                    bullet.body.originY = 1.0;
-                    bullet.body.velocity.y = -500;
-                    bullet.body.allowGravity = true;
-                    // make bullet visible
-                    bullet.setActive(true);
-                    bullet.setVisible(true);
-                    // setup callback to hide bullet when it reaches the end of the screen
-                    bullet.body.onWorldBounds = true;                    
-                    bullet.body.world.on('worldbounds', function(body) {
-                        if (body.gameObject == bullet) {
-                            bullet.setVisible(false);
-                            bullet.setActive(false);
-                        }
-                    })
-
-                }
+                fire_bullet(this.bullets_group, this.sprite.x, this.sprite.y - 50, -1);
             }
         },
     };
 }
 
-
-function create_enemies(num_horizontal = 5, x = 200, y = 540, max_vel = 25, bullet_image_id = "enemylaser",
-                        min_x = 10, max_x = 390) 
+/**
+ * Create a group of enemies
+ * @param {int} num_horizontal number of columns in the group 
+ * @param {int} x horizontal position of the group
+ * @param {int} y vertical position of the group
+ * @param {int} vertical_speed vertical speed (should be positive)
+ * @param {int} horizontal_speed horizontal speed (should be positive)
+ * @param {String} bullet_image_id image id for the enemy bullets
+ * @param {int} min_x minimum horizontal position for an enemy (when it reaches this edge, it switches direction)
+ * @param {int} max_x maximum horizontal position for an enemy (when it reaches this edge, it switches direction)
+ * @param {Array} num_rows number of rows per enemy (1, 2, and 3). Must be an array with 3 ints.
+ */
+function create_enemies(num_horizontal = 5, x = 200, y = 540, max_vel = 5, horizontal_speed = 10, 
+                        bullet_image_id = "enemylaser", min_x = 10, max_x = 390, num_rows = [1, 2, 2])
 {
+    var canvas_width = this.sys.canvas.width                                    //!< width of the canvas
+    var explosions = ['explosionpurple', 'explosionblue', 'explosiongreen'];    //!< name of explosion for each enemy (from 1 to 3)
+    var enemies = this.physics.add.group();                                     // group of enemies
 
-    this.anims.create({
-        key: 'enemy1_move',
-        frames: [
-            { key: 'enemy1_1' },
-            { key: 'enemy1_2' },
-        ],
-        frameRate: 2,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'enemy2_move',
-        frames: [
-            { key: 'enemy2_1' },
-            { key: 'enemy2_2' },
-        ],
-        frameRate: 2,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'enemy3_move',
-        frames: [
-            { key: 'enemy3_1' },
-            { key: 'enemy3_2' },
-        ],
-        frameRate: 2,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'enemy1_exp',
-        frames: [{ key: 'explosionpurple' }],
-        frameRate: 10,
-    });
-
-    this.anims.create({
-        key: 'enemy2_exp',
-        frames: [{ key: 'explosionblue' },],
-        frameRate: 10,
-    });
-
-    this.anims.create({
-        key: 'enemy3_exp',
-        frames: [{ key: 'explosiongreen' }],
-        frameRate: 10,
-    });
-
-    enemies = this.physics.add.group();
-    // add purple enemies
-    for (var i=0; i<num_horizontal; i++) {
-        enemy1 = this.physics.add.sprite(400, 300, 'enemy1_1').play('enemy1_move');
-        enemy1.setOrigin(0.5, 1.0);
-        enemy1.displayWidth = 50;
-        enemy1.scaleY = enemy1.scaleX;
-        enemy1.body.maxVelocity.y = max_vel;
-        enemy1.explote_anim = 'enemy1_exp';
-        enemies.add(enemy1);
+    // create animations for all enemies, animations for explosions, and groups of robots
+    for (var e=1; e<4; e++) {
+        // animation for the enemy
+        this.anims.create({
+            key: 'enemy' + e + '_move',
+            frames: [
+                { key: 'enemy' + e + '_1' },
+                { key: 'enemy' + e + '_2' },
+            ],
+            frameRate: 2,
+            repeat: -1
+        });
+        // animation for the enemy explosion
+        this.anims.create({
+            key: 'enemy' + e + '_exp',
+            frames: [{ key: explosions[e - 1] }],
+            frameRate: 10,
+        });
+        // add actual enemies to the enemies group 
+        for (var i=0; i<num_horizontal * num_rows[e - 1]; i++) {
+            enemy = this.physics.add.sprite(400, 300, 'enemy' + e + '_1').play('enemy' + e + '_move');
+            enemy.setOrigin(0.5, 1.0);
+            enemy.displayWidth = 50;
+            enemy.scaleY = enemy.scaleX;
+            enemy.body.maxVelocity.y = max_vel;
+            enemy.explote_anim = 'enemy' + e + '_exp';
+            // add extra parameters to know what is the position of the enemy in the grid
+            enemy.grid_row = Math.floor(i / num_horizontal);
+            for (var j=e-2; j >= 0; j--) {
+                enemy.grid_row += num_rows[j];
+            }
+            enemy.grid_column = i % num_horizontal;
+            enemies.add(enemy);
+        }
     }
-    // add cyan enemies
-    for (var i=0; i<num_horizontal*2; i++) {
-        enemy2 = this.physics.add.sprite(400, 300, 'enemy2_1').setScale(0.5).play('enemy2_move');
-        enemy2.setOrigin(0.5, 1.0);
-        enemy2.displayWidth = 50;
-        enemy2.scaleY = enemy2.scaleX;
-        enemy2.body.maxVelocity.y = max_vel;
-        enemy2.explote_anim = 'enemy2_exp';
-        enemies.add(enemy2);
-    }
-    // add green enemies
-    for (var i=0; i<num_horizontal*2; i++) {
-        enemy3 = this.physics.add.sprite(400, 300, 'enemy3_1').setScale(0.5).play('enemy3_move');
-        enemy3.setOrigin(0.5, 1.0);
-        enemy3.displayWidth = 50;
-        enemy3.scaleY = enemy3.scaleX;
-        enemy3.body.maxVelocity.y = max_vel;
-        enemy3.explote_anim = 'enemy3_exp';
-        enemies.add(enemy3);
-    }
-    // align in a grid
+    // align group enemies in a grid
     Phaser.Actions.GridAlign(enemies.getChildren(), 
-    { width: num_horizontal, height: 5, cellWidth: 50, cellHeight: 50, position: Phaser.Display.Align.CENTER, x: 100, y: 0 });
-
+    { width: num_horizontal, height: 5, cellWidth: 60, cellHeight: 50, position: Phaser.Display.Align.CENTER, x: 100, y: 0 });
+    // set initial velocity for group
     Phaser.Actions.Call(enemies.getChildren(), function(e) {
-        e.setVelocityX(-50)
+        e.setVelocityX(-horizontal_speed)
     })
-    canvas_width = this.sys.canvas.width
+    // store number of columns in the grid
+    var children = enemies.getChildren();
+    enemies.num_columns = num_horizontal;
+    enemies.num_rows = children[children.length - 1].grid_row;
+
+    // create bullets pool
+    var bullets = this.create_bullets_pool(30, bullet_image_id, true);
+
+    // create timer to fire enemy bullets
+    enemies.fire_timer = this.time.addEvent({ delay: Phaser.Math.Between(500, 1000), loop: true, 
+                                              callback: () => { fire_enemy_bullet(enemies, bullets) } });
 
     return {
-        enemies_group: enemies,     //!< enemies group
+        enemies_group: enemies,                //!< enemies group
+        bullets_group: bullets,                //!< bullets group
         update(margin = 10)                    //!< update the enemies state
         {
             // move right?
@@ -174,7 +237,7 @@ function create_enemies(num_horizontal = 5, x = 200, y = 540, max_vel = 25, bull
             });
             if (typeof left_enemy != 'undefined') {
                 Phaser.Actions.Call(this.enemies_group.getChildren(), function(e) {
-                    e.setVelocityX(50)
+                    e.setVelocityX(horizontal_speed)
                 })
             } else { // move left?
                 right_enemy = this.enemies_group.getChildren().find(function(e){
@@ -182,13 +245,10 @@ function create_enemies(num_horizontal = 5, x = 200, y = 540, max_vel = 25, bull
                 });
                 if (typeof right_enemy != 'undefined') {
                     Phaser.Actions.Call(this.enemies_group.getChildren(), function(e) {
-                        e.setVelocityX(-50)
+                        e.setVelocityX(-horizontal_speed)
                     })
                 }
             }
-
-            // todo. make the enemies shoot bullets
-            
         },
     };
 }
@@ -205,6 +265,7 @@ var config = {
         debug: true,
         fps: 30,
         arcade: {
+            debug: false, // set to true to enable physics visualization
             gravity: { y: 100 }
         }
     },
@@ -213,6 +274,7 @@ var config = {
         create: create,
         update: update,
         extend: {
+            create_bullets_pool: create_bullets_pool,
             create_ship: create_ship,
             create_enemies: create_enemies,
         }
@@ -256,6 +318,8 @@ function create ()
     ship1 = this.create_ship("ship", this.sys.canvas.width / 4, 540);
     enemies1 = this.create_enemies(5, this.sys.canvas.width / 4, this.sys.canvas.height / 8); 
 
+    // add colliders
+    // first, take care of the bullets fired by the player
     this.physics.add.collider(enemies1.enemies_group, ship1.bullets_group, (enemy, bullet) => {
         // destroy the enemy
         enemy.play(enemy.explote_anim, true);
@@ -265,7 +329,22 @@ function create ()
         // hide the bullet 
         bullet.body.x = this.sys.canvas.width;
         bullet.body.y = this.sys.canvas.height;
-    })
+    });
+    this.physics.add.collider(enemies1.bullets_group, ship1.bullets_group, (enemy_bullet, ship_bullet) => {
+        // hide both bullets 
+        enemy_bullet.body.x = this.sys.canvas.width;
+        enemy_bullet.body.y = this.sys.canvas.height;
+        ship_bullet.body.x = this.sys.canvas.width;
+        ship_bullet.body.y = this.sys.canvas.height;
+    });
+    // second, let's take care of the bullets fired by the enemies
+    this.physics.add.collider(ship1.sprite, enemies1.bullets_group, (ship_sprite, bullet) => {
+        // hide the bullet 
+        bullet.body.x = this.sys.canvas.width;
+        bullet.body.y = this.sys.canvas.height;
+        // kill the enemy. The change in behavior takes place within the update function of the ship
+        ship_sprite.props.dead = true;
+    });
 }
 
 function update ()
