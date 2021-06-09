937679a89bfe4e1be803c8c302977a7565eab40a
Kate Candon <kate.candon@yale.edu>
2021-06-09 17:13:15 -0400

    update dodge logic so ai_agent wastes less time

--- space_invaders/agents/uncooperative.py
+++ space_invaders/agents/uncooperative.py
@@ -9,12 +9,21 @@ class Uncooperative:
         return False
 
     def update(self, state):
-        SHIP_Y = 540
-        CANVAS = 800
+        # set variable values
+        CANVAS = 800 # width of canvas
+        MIDDLE = 400 # middle of x axis
+        SHOOTING_RANGE = 24 # hit enemy if within range
+        HIT_RANGE = 35 # range for dodging bullets
+        SECOND_HIT_RANGE = 50 # range check for moving towards enemies
 
+        SHIP_Y = 540 # y value of ai_ship
+        VERTICAL_BUFFER = 440 # vertical buffer for bullets (higher numbers means closer to ship)
+
+        AI_RELATIVE_SPEED = 0.6 # relatively how much shorter time between bullets is for ai_agent
+        FREQUENCY_BOUND = 500 # ai max time between shots
+
+        # gather state information
         s = state
-
-        #frame_number = s['frame_number']
 
         ship_x = s['ai_position']
 
@@ -27,11 +36,10 @@ class Uncooperative:
         ai_shoots = s['can_shoot']
 
         ai_last_shot = s['ai_last_shot_time']
-        player_average_frequency = 1500
-        if s['player_avg_frequency']:
-            player_average_frequency = min(player_average_frequency, int(s['player_avg_frequency']))
 
-        #print(f"player_average_frequency: {player_average_frequency}")
+        player_avg_frequency = FREQUENCY_BOUND
+        if s['player_avg_frequency']:
+            player_avg_frequency = min(FREQUENCY_BOUND, int(s['player_avg_frequency']))
 
         num_left_enemies = len(enemies_left_positions)
         num_right_enemies = len(enemies_right_positions)
@@ -43,40 +51,29 @@ class Uncooperative:
         shoot = False
         hit = False
 
+        # find bullet that should determine movement
         nearest_bullet = [0,0]
 
-        if ship_x >= 400:
+        if ship_x >= MIDDLE:
             bullets_to_search = bullets_right_positions
         else:
             bullets_to_search = bullets_left_positions
         
-        if ship_x >= 375 and ship_x <= 425:
+        if ship_x >= MIDDLE - 25 and ship_x <= MIDDLE +25:
             bullets_to_search = bullets_left_positions + bullets_right_positions
 
-        for bullet in bullets_to_search:
-            x_diff = abs(bullet[0] - ship_x)
-            if bullet[1] > SHIP_Y or x_diff > 200:
-                bullets_to_search.remove(bullet)
+        problem_bullets = []
+        x_diff_prev = CANVAS
 
         for bullet in bullets_to_search:
             x_diff = abs(bullet[0] - ship_x)
-            x_diff_prev = abs(nearest_bullet[0] - ship_x)
-            # h_dist_to_prev_bullet = abs(nearest_bullet[0] - ship_x)
-            if bullet[1] > nearest_bullet[1]:
-                nearest_bullet = bullet
-            elif bullet[1] > SHIP_Y - 125 and x_diff < x_diff_prev:
-                nearest_bullet = bullet
-        
-        #print(nearest_bullet)
+            if bullet[1] < SHIP_Y and bullet[1] > VERTICAL_BUFFER and x_diff < HIT_RANGE * 2:
+                problem_bullets.append(bullet)
+                if x_diff < x_diff_prev:
+                    nearest_bullet = bullet
+                    x_diff_prev = x_diff
 
-        # h_dist_to_bullet = float('inf')
-        # for bullet in bullets_to_search:
-        #     d = abs(bullet[0] - ship_x)
-        #     if d < h_dist_to_bullet:
-        #        h_dist_to_bullet = d
-        #        nearest_bullet = bullet
-        #fadsFASDJFKASD
-
+        # find nearest enemy
         nearest_enemy = [0,0]
         nearest_x_diff = CANVAS
 
@@ -92,20 +89,10 @@ class Uncooperative:
                 nearest_x_diff = check_distance
 
         # set restriction on frequency based on player's recent average
-        #if frame_number - ai_last_shot < player_average_frequency * 0.8:
-        #print(f"{round(time.time() * 1000)} - {ai_last_shot} < {player_average_frequency * 0.8}")
-        #print(f"{round(time.time() * 1000) - ai_last_shot} < {player_average_frequency * 0.8}")
-        if round(time.time() * 1000) - ai_last_shot < player_average_frequency * 0.8:
+        if round(time.time() * 1000) - ai_last_shot < player_avg_frequency * AI_RELATIVE_SPEED:
             ai_shoots = False 
 
-        # print (f" Y CONDITION: {nearest_bullet[1] > SHIP_Y - 350}")
-        # print (f" Y_DIFF: {SHIP_Y - nearest_bullet[1]}, X_DIFF: {ship_x- nearest_bullet[0]}")
-
-        HIT_RANGE = 40 # range for dodging bullets
-        SECOND_HIT_RANGE = 75 # range check for moving towards enemies
-
-        #print("AI SHOOTS: ", ai_shoots)
-
+        # check if ai_agent is in danger of being hit by bullet
         if nearest_bullet[0] <= ship_x + HIT_RANGE and nearest_bullet[0] >= ship_x - HIT_RANGE:
             hit = True
 
@@ -113,6 +100,7 @@ class Uncooperative:
 
         if not(ai_shoots):
             #print("cant shoot")
+            # if can't shoot, figure out which way to move
             if hit:
                 if nearest_bullet[0] >= CANVAS-75:
                     left = True
@@ -124,7 +112,7 @@ class Uncooperative:
                     right = True
         else:
             #IF AI CAN SHOOT
-            if nearest_x_diff <= 24:
+            if nearest_x_diff <= SHOOTING_RANGE:
                 shoot = True
 
             if nearest_enemy[0] < ship_x:
@@ -139,7 +127,10 @@ class Uncooperative:
                     approached_enemy = True
                     
             if not approached_enemy and hit:
-                #print("GOTTA MOVE")
+                if x_diff_prev >5:
+                    # if not going to hit bullet, don't shoot so you can move
+                    shoot = False
+                # figure out which way to move so you don't get hit
                 if nearest_bullet[0] >= CANVAS-75:
                     left = True
                 elif nearest_bullet[0] <= self.min_x + 55:
@@ -149,10 +140,12 @@ class Uncooperative:
                 elif nearest_bullet[0] <= ship_x:
                     right = True
 
+        # if no more enemies to worry about, do nothing
         if num_right_enemies==0 and not(attack_left):
             left = False
             right = False
 
+        # pass back action
         action = {
             'left': left,
             'right': right,
